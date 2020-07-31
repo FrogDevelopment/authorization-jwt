9ad68b6bc8113df3e945d3fd45e4a9a39ea5d0a6
LE GALL Benoît <legall.benoit@gmail.com>
2020-07-31 10:26:34 +0200

    feat: include token claims data on Authentication

--- src/main/java/fr/frogdevelopment/jwt/JwtAuthenticationToken.java
+++ src/main/java/fr/frogdevelopment/jwt/JwtAuthenticationToken.java
@@ -1,27 +1,77 @@
 package fr.frogdevelopment.jwt;
 
+import static java.util.Collections.emptyList;
+import static java.util.stream.Collectors.toUnmodifiableList;
+import static java.util.stream.Collectors.toUnmodifiableMap;
+
+import io.jsonwebtoken.Claims;
 import java.util.Collection;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
-public class JwtAuthenticationToken extends AbstractAuthenticationToken {
+public class JwtAuthenticationToken implements Authentication {
+
+    static final String AUTHORITIES_KEY = "authorities";
 
     private final String principal;
+    private final Collection<GrantedAuthority> authorities;
+    private final Map<String, Object> details;
+    private boolean authenticated;
 
-    JwtAuthenticationToken(String principal, Collection<SimpleGrantedAuthority> authorities) {
-        super(authorities);
-        this.principal = principal;
-        super.setAuthenticated(true);
-    }
-
-    @Override
-    public String getCredentials() {
-        return null;
+    JwtAuthenticationToken(Claims claims) {
+        this.principal = claims.getSubject();
+        //noinspection unchecked
+        this.authorities = ((List<String>) claims.getOrDefault(AUTHORITIES_KEY, emptyList()))
+                .stream()
+                .map(SimpleGrantedAuthority::new)
+                .collect(toUnmodifiableList());
+        this.details = claims.entrySet()
+                .stream()
+                .collect(toUnmodifiableMap(Entry::getKey, Entry::getValue));
+        this.authenticated = true;
     }
 
     @Override
     public String getPrincipal() {
         return principal;
+    }
+
+    @Override
+    public String getName() {
+        return getPrincipal();
+    }
+
+    @Override
+    public Collection<GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public Map<String, Object> getDetails() {
+        return details;
+    }
+
+    public Object getDetail(String key) {
+        return details.get(key);
+    }
+
+    @Override
+    public boolean isAuthenticated() {
+        return authenticated;
+    }
+
+    @Override
+    public void setAuthenticated(boolean authenticated) {
+        this.authenticated = authenticated;
+    }
+
+    @Override
+    public String getCredentials() {
+        return null;
     }
 
 }
