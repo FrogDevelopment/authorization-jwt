9b3b7b58138d6e4eebbe4e155f20ac8e8303f2d3
FrogDevelopper <legall.benoit@gmail.com>
2020-10-14 11:07:59 +0200

    feat: add condition to autoconfiguration

--- src/main/java/com/frogdevelopment/jwt/JwtAuthorizationAutoConfiguration.java
+++ src/main/java/com/frogdevelopment/jwt/JwtAuthorizationAutoConfiguration.java
@@ -1,10 +1,12 @@
 package com.frogdevelopment.jwt;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ConditionalOnProperty("security.jwt.")
 public class JwtAuthorizationAutoConfiguration {
 
     private final String signingKey;
